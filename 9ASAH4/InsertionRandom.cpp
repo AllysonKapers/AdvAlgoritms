a8dce04565a1073ed0886e8e5d4638cbef0ee2f8
SethAllen-ai <77354497+SethAllen-ai@users.noreply.github.com>
2021-11-22 13:34:42 -0600

    Add files via upload

---
+++ 9ASAH4/InsertionRandom.cpp
@@ -0,0 +1,91 @@
+//Seth Allen
+//Paxton Proctor
+//Ally Moore
+//Assemble 1 File RandomOrder.txt with how ever
+//elements you want to process and click run
+#include <iostream>
+#include <fstream>
+#include <iomanip>   //fix, setw
+#include <stdlib.h> //srand, rand
+#include <chrono>
+#include <time.h>    //clock, clock_t
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+/* https://www.geeksforgeeks.org/insertion-sort/ */
+/* Function to sort an array using insertion sort*/
+void insertionSort(int arr[], int n)
+{
+	int i, key, j;
+	for (i = 1; i < n; i++)
+	{
+
+		key = arr[i];
+		j = i - 1;
+		/* Move elements of arr[0..i-1], that are
+		greater than key, to one position ahead
+		of their current position */
+		while (j >= 0 && arr[j] > key)
+		{
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+		arr[j + 1] = key;
+	}
+}
+
+int main() {
+	ifstream infile("RandomOrder.txt");
+	int temp;
+	int nx = 0;
+
+	while (infile >> temp)
+	{
+		//cout << temp<<" "<<endl;
+		nx++;
+	}
+	cout << "Total:" << nx << endl;
+
+	infile.close();
+	infile.open("RandomOrder.txt");
+
+	int* A = new int[nx]();
+
+	for (size_t i = 0; i<nx && infile >> A[i]; i++)
+		infile.close();
+
+	for (size_t i = 0; i < nx - 1; i++)
+	{
+		for (size_t j = i; j < nx; j++)
+		{
+			if (A[i] > A[j])
+			{
+				int temp = A[i];
+				A[i] = A[j];
+				A[j] = temp;
+			}
+		}
+	}
+
+	using Clock = chrono::high_resolution_clock;
+
+	clock_t start, stop;
+
+	long long Selectionelapsedtime;
+	long long Insertionelapsedtime;
+
+	start = Clock::now().time_since_epoch().count();
+
+	insertionSort(A, nx);
+
+	stop = Clock::now().time_since_epoch().count();
+	Insertionelapsedtime = (long long)(stop - start);
+
+	cout << "Insertiontime in nanoseconds: " << Insertionelapsedtime << '\n';
+
+	delete[] A;
+	system("pause");
+	return 0;
+}
---
+++ 9ASAH4/SelectionAscending.cpp
@@ -0,0 +1,91 @@
+//Seth Allen
+//Paxton Proctor
+//Ally Moore
+//Assemble 1 File AscendingOrder.txt with how ever
+//elements you want to process and click run
+#include <iostream>
+#include <fstream>
+#include <iomanip>   //fix, setw
+#include <stdlib.h> //srand, rand
+#include <chrono>
+#include <time.h>    //clock, clock_t
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+/* https://www.geeksforgeeks.org/selection-sort/ */
+/* functions for selection sort*/
+void swap(int* xp, int* yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+void selectionSort(int arr[], int n)
+{
+	int i, j, min_idx;
+
+	// One by one move boundary of unsorted subarray
+	for (i = 0; i < n - 1; i++)
+	{
+		// Find the minimum element in unsorted array
+		min_idx = i;
+		for (j = i + 1; j < n; j++)
+			if (arr[j] < arr[min_idx])
+				min_idx = j;
+		// Swap the found minimum element with the first element
+		swap(&arr[min_idx], &arr[i]);
+	}
+}
+
+int main() {
+	ifstream infile("AscendingOrder.txt");
+	int temp;
+	int nx = 0;
+
+	while (infile >> temp) {
+		//cout << temp<<" "<<endl;
+		nx++;
+	}
+	cout << "Total:" << nx << endl;
+
+	infile.close();
+	infile.open("AscendingOrder.txt");
+
+	int* A = new int[nx]();
+
+	for (size_t i = 0; i<nx && infile >> A[i]; i++)
+		infile.close();
+
+	for (size_t i = 0; i < nx - 1; i++) {
+		for (size_t j = i; j < nx; j++) {
+			if (A[i] > A[j]) {
+				int temp = A[i];
+				A[i] = A[j];
+				A[j] = temp;
+			}
+		}
+	}
+
+	using Clock = chrono::high_resolution_clock;
+
+	clock_t start, stop;
+
+	long long Selectionelapsedtime;
+	long long Insertionelapsedtime;
+
+	start = Clock::now().time_since_epoch().count();
+
+	selectionSort(A, nx);
+
+	stop = Clock::now().time_since_epoch().count();
+	Selectionelapsedtime = (long long)(stop - start);
+
+	cout << "Selectiontime in nanoseconds: " << Selectionelapsedtime << '\n';
+	delete[] A;
+
+	system("pause");
+	return 0;
+}
